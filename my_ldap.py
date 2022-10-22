e755fb1d3612ee3e9e2648919204875304c83024
Israel Efrati <100380924+IsraelEfrati@users.noreply.github.com>
2022-10-22 22:18:26 +0300

    Add files via upload

--- my_ldap.py
+++ my_ldap.py
@@ -1,15 +1,20 @@
+import os.path
+
 from ldap3 import Server, Connection, ALL, SUBTREE, SAFE_SYNC
 from ldap3.core.exceptions import LDAPException, LDAPBindError
 from pyotp import random_base32, totp
 
+LDAP_IP = ""
 
 def get_secret():
-    with open("code.txt") as f:
-        line = f.readline()
-    if line:
-        secret = line
+    if os.path.exists("code.txt"):
+        with open("code.txt", "r") as f:
+            secret = f.readline()
     else:
         secret = random_base32()
+        with open("code.txt", "w") as f:
+            f.write(secret)
+
     return secret
 
 
@@ -18,9 +23,10 @@ OTP = totp.TOTP(SECRET)
 
 
 def connect_ldap_server(full_name, password):
+    global LDAP_IP
     try:
         # Provide the hostname and port number of the openLDAP
-        server_uri = f"ldap://192.168.25.130:389"
+        server_uri = "ldap://{0}:389".format(LDAP_IP)
         server = Server(server_uri, get_info=ALL)
         # username and password can be configured during openldap setup
         user = "cn={},cn=my_users,dc=example,dc=org".format(full_name)
